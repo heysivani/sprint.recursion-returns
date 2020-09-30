818ae0644da45cf2249bce09ba2b71298135665b
heysivani <heysivani@gmail.com>
2020-10-01 00:04:07 +0900

    robotPath passes 5 tests; TODO fix toggling in recursive function

--- src/robotPaths.js
+++ src/robotPaths.js
@@ -1,6 +1,7 @@
 class Board {
   constructor(size) {
     this.board = [];
+    this.target = size - 1;
     for (let row = 0; row < size; row += 1) {
       this.board.push([]);
       for (let col = 0; col < size; col += 1) {
@@ -13,8 +14,19 @@ class Board {
     this.board[row][col] = !this.board[row][col];
     return this.board;
   }
+
   hasBeenVisited(row, col) {
     return this.board[row][col];
+  }
+
+  resetBoard() {
+    for (let r = 0; r <= this.target; r++) {
+      for (let c = 0; c <= this.target; c++) {
+        if (this.hasBeenVisited(r, c)) {
+          this.togglePiece(r, c);
+        }
+      }
+    }
   }
 }
 
@@ -28,74 +40,80 @@ class RobotPaths {
     this.goal = size - 1;
     this.pathCount = 0;
     this.size = size;
-    const moves = [];
+  }
+
+  counter() {
+    this.pathCount += 1;
+    console.log(`counter updated to ${this.pathCount}`);
   }
 
   solve() {
-    console.log("STARTING goal " + this.goal);
-    console.log("I'm SOLVING");
-    let moveRobot = (row, column) => {
-      console.log("CURRENT ROW " + row + "CURRENT COLUMN " + column);
+    console.log(`STARTING goal ${this.goal}`);
+
+    const moveRobot = (row, column) => {
+      console.log(`CURRENT ROW  ${row} CURRENT COLUMN  ${column}`);
 
       // WIN and toggle
-      console.log("GOAL " + this.goal + " row " + row + " column " + column);
       if (row === this.goal && column === this.goal) {
+        console.log(
+          `GOAL OF ${this.goal} REACHED at row ${row} column ${column}`
+        );
         console.log("WE WON");
-        this.pathCount += 1;
-        for (let row = 0; row < n; row++) {
-          for (let col = 0; col < n; col++) {
-            if (this.board[row][column]) {
-              this.board.togglePiece(row, column);
-            }
-          }
-        }
+        this.counter();
+        //this.board.resetBoard();
         return;
       }
       // DOWN A ROW
-      if (row + 1 < this.goal && row + 1 >= 0) {
-        console.log("GOING DOWN");
+      if (row + 1 <= this.goal && row + 1 >= 0) {
+        console.log("TRY GOING DOWN?");
         // has been visited?
         if (!this.board.hasBeenVisited(row + 1, column)) {
-          console.log("HASN'T BEEN VISITED");
-          this.board.togglePiece(row + 1, column);
+          console.log("down OK!");
+          this.board.togglePiece(row, column);
           moveRobot(row + 1, column);
         }
       }
 
       // UP A ROW
-      if (row - 1 < this.goal && row - 1 >= 0) {
-        console.log("GOING DOWN");
+      if (row - 1 <= this.goal && row - 1 >= 0) {
+        console.log("TRY GOING UP?");
         // has been visited?
         if (!this.board.hasBeenVisited(row - 1, column)) {
-          this.board.togglePiece(row - 1, column);
+          console.log("up OK!");
+          this.board.togglePiece(row, column);
           moveRobot(row - 1, column);
         }
       }
 
       // LEFT
-      if (column - 1 < this.goal && column - 1 >= 0) {
+      if (column - 1 <= this.goal && column - 1 >= 0) {
+        console.log("TRY GOING LEFT?");
         // has been visited?
         if (!this.board.hasBeenVisited(row, column - 1)) {
-          this.board.togglePiece(row, column - 1);
+          console.log("left OK!");
+          this.board.togglePiece(row, column);
           moveRobot(row, column - 1);
         }
       }
 
       // RIGHT
-      if (column + 1 < this.goal && column + 1 >= 0) {
+      if (column + 1 <= this.goal && column + 1 >= 0) {
+        console.log("TRY GOING RIGHT");
         // has been visited?
         if (!this.board.hasBeenVisited(row, column + 1)) {
-          this.board.togglePiece(row, column + 1);
+          console.log("right OK!");
+          this.board.togglePiece(row, column);
           moveRobot(row, column + 1);
         }
       }
 
-      return;
+      //return;
     };
 
     moveRobot(0, 0);
-    console.log("RESULT " + this.pathCount);
-    return this.pathCount;
+    const result = this.pathCount;
+    console.log(`RESULT ${result}`);
+    return result;
   }
 }
 
